88ae5e84b71555b7a21df4f95daca6cc54fd5726
kEERill <kirill.popkov.work@gmail.com>
2024-09-15 22:57:46 +0300

    [init] Наброски

---
+++ src/Connections/KafkaConnection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Micromus\KafkaBus\Connections;
+
+use Micromus\KafkaBus\Consumers\ConsumerConfiguration;
+use Micromus\KafkaBus\Contracts\Connections\Connection;
+use Micromus\KafkaBus\Contracts\Consumers\Consumer;
+use Micromus\KafkaBus\Contracts\Producers\Producer;
+use Micromus\KafkaBus\Producers\ProducerConfiguration;
+
+class KafkaConnection implements Connection
+{
+
+    public function createProducer(string $topic, ProducerConfiguration $configuration): Producer
+    {
+        // TODO: Implement createProducer() method.
+    }
+
+    public function createConsumer(string $topic, ConsumerConfiguration $configuration): Consumer
+    {
+        // TODO: Implement createConsumer() method.
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Micromus\KafkaBus\Tests;
+
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
