2a8ab851bec4343eef469d1e12fb4e22b0edf944
kEERill <kEERill@users.noreply.github.com>
2024-10-02 13:25:58 +0000

    Fix styling

--- src/Bus/Listeners/Groups/Group.php
+++ src/Bus/Listeners/Groups/Group.php
@@ -6,9 +6,8 @@ readonly class Group
 {
     public function __construct(
         public GroupRoutes $routes,
-        public Options $options = new Options(),
+        public Options $options = new Options,
         public int $maxMessages = -1,
         public int $maxTime = -1
-    ) {
-    }
+    ) {}
 }
